19c37888ed8382cd1c173b615a426b5f937c603a
Floro Lorenzo Gagni <enzopro2222@gmail.com>
2025-05-18 11:01:03 +0800

    deleting unecessary files

--- hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-$hashed_password = password_hash("12345", PASSWORD_DEFAULT);
-echo $hashed_password;
-?>
--- testhash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = "admin321";
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "Hashed Password: " . $hash;
-?>
